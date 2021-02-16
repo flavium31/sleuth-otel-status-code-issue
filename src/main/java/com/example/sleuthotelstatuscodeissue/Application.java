b6c7a1e9db4b939f2c1e21b1b5fd130d5f0d27f8
flaviu.muresan@maersk.com <w7og3bljlmmn742puqw2zwzu7ycuwmflr2nb4swrwzivccx6arpq>
2021-02-16 15:58:27 +0100

    Add sleuth OTel integration

--- src/main/java/com/example/sleuthotelstatuscodeissue/Application.java
+++ src/main/java/com/example/sleuthotelstatuscodeissue/Application.java
@@ -1,10 +1,28 @@
 package com.example.sleuthotelstatuscodeissue;
 
+import io.opentelemetry.api.trace.TracerProvider;
+import io.opentelemetry.exporter.logging.LoggingSpanExporter;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+import io.opentelemetry.sdk.trace.samplers.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+
+	@Bean
+	public TracerProvider tracerProvider() {
+		return SdkTracerProvider.builder()
+				.addSpanProcessor(SimpleSpanProcessor.create(new LoggingSpanExporter()))
+				.build();
+	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.alwaysOn();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
